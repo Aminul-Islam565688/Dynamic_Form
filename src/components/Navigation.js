99aa56ee9c7b0ea2fdab068fcc95f69a3343f47c
x-aminulislam-x <aminulislam1developer@gmail.com>
2022-01-05 11:23:44 +0600

    flex problem solved

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -45,93 +45,95 @@ function Navigation({ addNewPage, preview, handleUploadOpen, otherPage }) {
   // now it's in x-aminulislam-x repository
 
   return (
-    <header className="header-area container-fluid">
-      <div className="header-left">Automo</div>
+    <>
+      <header className="header-area container-fluid">
+        <div className="header-left">Automo</div>
 
-      <div className="header-middle">
-        <ul>
-          {!otherPage ? (
-            <>
-              <li>
-                <FontAwesomeIcon icon={["fas", "plus"]} />
-                <span>Insert</span>
-              </li>
-
-              <li onClick={() => dispatch(add_new_page())}>
-                <FontAwesomeIcon icon={["far", "file-alt"]} />
-                <span>New Page</span>
-              </li>
-
-              <li onClick={() => handleUploadOpen("logo")}>
-                <FontAwesomeIcon icon={["fab", "pied-piper"]} />
-                <span>Logo</span>
-              </li>
-
-              <li onClick={() => handleUploadOpen("cover photo")}>
-                <FontAwesomeIcon icon={["far", "image"]} />
-                <span>Cover</span>
-              </li>
-
-              <li onClick={preview}>
-                <FontAwesomeIcon icon={["far", "eye"]} />
-                <span>Preview</span>
-              </li>
-
-              <li onClick={fromPublish}>
-                <FontAwesomeIcon icon={["far", "paper-plane"]} />
-                <span>Publish</span>
-              </li>
-
-              <li onClick={() => { ShareLink(); notify() }} >
-                <FontAwesomeIcon icon={["fas", "share-alt"]} />
-                <span>Share</span>
-              </li>
-
-              <li>
-                <FontAwesomeIcon icon={["fas", "cog"]} />
-                <span>Settings</span>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to={`/form/${formId}`}>
+        <div className="header-middle">
+          <ul>
+            {!otherPage ? (
+              <>
+                <li>
                   <FontAwesomeIcon icon={["fas", "plus"]} />
-                  <span>Create Form</span>
-                </Link>
-              </li>
+                  <span>Insert</span>
+                </li>
 
-              <li>
-                <Link to="/drag-and-drop">
-                  <FontAwesomeIcon icon={["fas", "grip-vertical"]} />
-                  <span>Drag and Drop</span>
-                </Link>
-              </li>
+                <li onClick={() => dispatch(add_new_page())}>
+                  <FontAwesomeIcon icon={["far", "file-alt"]} />
+                  <span>New Page</span>
+                </li>
 
-              <li>
-                <Link to="/login">
-                  <FontAwesomeIcon icon={["fas", "lock"]} />
-                  <span>Login</span>
-                </Link>
-              </li>
+                <li onClick={() => handleUploadOpen("logo")}>
+                  <FontAwesomeIcon icon={["fab", "pied-piper"]} />
+                  <span>Logo</span>
+                </li>
 
-              <li>
-                <Link to="/registration">
-                  <FontAwesomeIcon icon={["far", "clipboard"]} />
-                  <span>Sign up</span>
-                </Link>
-              </li>
-            </>
-          )}
-        </ul>
-      </div>
+                <li onClick={() => handleUploadOpen("cover photo")}>
+                  <FontAwesomeIcon icon={["far", "image"]} />
+                  <span>Cover</span>
+                </li>
 
-      <div className="header-right">
-        <span>
-          <Link to="/"> <FontAwesomeIcon icon={["fas", "th-large"]} /> Event Manage</Link>
-        </span>
-        <FontAwesomeIcon icon={["fas", "user-circle"]} />
-      </div>
+                <li onClick={preview}>
+                  <FontAwesomeIcon icon={["far", "eye"]} />
+                  <span>Preview</span>
+                </li>
+
+                <li onClick={fromPublish}>
+                  <FontAwesomeIcon icon={["far", "paper-plane"]} />
+                  <span>Publish</span>
+                </li>
+
+                <li onClick={() => { ShareLink(); notify() }} >
+                  <FontAwesomeIcon icon={["fas", "share-alt"]} />
+                  <span>Share</span>
+                </li>
+
+                <li>
+                  <FontAwesomeIcon icon={["fas", "cog"]} />
+                  <span>Settings</span>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to={`/form/${formId}`}>
+                    <FontAwesomeIcon icon={["fas", "plus"]} />
+                    <span>Create Form</span>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="/drag-and-drop">
+                    <FontAwesomeIcon icon={["fas", "grip-vertical"]} />
+                    <span>Drag and Drop</span>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="/login">
+                    <FontAwesomeIcon icon={["fas", "lock"]} />
+                    <span>Login</span>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="/registration">
+                    <FontAwesomeIcon icon={["far", "clipboard"]} />
+                    <span>Sign up</span>
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+
+        <div className="header-right">
+          <span>
+            <Link to="/"> <FontAwesomeIcon icon={["fas", "th-large"]} /> Event Manage</Link>
+          </span>
+          <FontAwesomeIcon icon={["fas", "user-circle"]} />
+        </div>
+      </header >
       <ToastContainer position="bottom-right"
         autoClose={5000}
         hideProgressBar
@@ -142,7 +144,7 @@ function Navigation({ addNewPage, preview, handleUploadOpen, otherPage }) {
         draggable
         theme='colored'
         pauseOnHover />
-    </header >
+    </>
   );
 }
 
